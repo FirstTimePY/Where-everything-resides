58eab662021be484dcbd1b142b5b0dd68dca781a
python beginner <139918720+FirstTimePY@users.noreply.github.com>
2023-07-19 00:34:29 +0300

    Add files via upload

---
+++ type_casting.py
@@ -0,0 +1,23 @@
+# type casting = convers the data type of a value to another data type
+
+x = 1 # int
+y = 2.0 # float
+z = "3" # str
+
+
+
+# convert x to a float
+#x = float(x)
+
+# perm change
+#y = int(y)
+
+# cast z as an int
+#z = int(z)
+
+
+# print("X is " + str(x))
+# non-perm change
+# print(int(y))
+# print "3" three times
+# print(z*3)
