b351ba85fc0e85d3355dc6c0b09f27854a9fa410
python beginner <139918720+FirstTimePY@users.noreply.github.com>
2023-07-19 00:26:54 +0300

    Add files via upload

---
+++ multiple_assigment.py
@@ -0,0 +1,29 @@
+### multiple assigment = allows us to assign many vars at the same time in 1 line
+
+
+# single assigment example:
+
+# name = "joe"
+# age = 21
+# attractive = True
+# print(name)
+# print(age)
+# print(attractive)
+
+
+# multiple assigment:
+
+# name, age, attractive = "Joe", 21, True
+# print(name)
+# print(age)
+# print(attractive)
+
+
+# 2nd example:
+
+# Spongebob = Patrick = Sandy = Squidward = 30
+
+# print(Spongebob)
+# print(Patrick)
+# print(Sandy)
+# print(Squidward)
---
+++ string_methods.py
@@ -0,0 +1,32 @@
+# Name of user:
+name = "joE mAMa"
+
+# length of the name's value/the name itself:
+#print(len(name))
+
+# Find the index of the first letter in the name:
+#print(name.find("j"))
+
+# Capitalizes the first letter of the name
+#print(name.capitalize())
+
+# Makes the whole name uppercase
+#print(name.upper())
+
+# Makes the whole name lowercase
+#print(name.lower())
+
+# Logs if the name is a digit or not
+#print(name.isdigit())
+
+# Logs if the name is alphabetical letters (side note: no they arents cuz of the space)
+#print(name.isalpha())
+
+# pretty much just like name.find()
+#print(name.count("o"))
+
+# replaces the letter "o" in the name with "a"
+#print(name.replace("o","a"))
+
+# repeat the name 3 times:
+#print(name*3)
