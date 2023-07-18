084078a35a981884bb834c55a9d6222053be6f5a
1st time py user <139918720+FirstTimePY@users.noreply.github.com>
2023-07-18 23:16:12 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,131 @@
+### Learning variables and everything related such as floating numbers, booleans and more
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Creating my first variable, this variable will represent the name of the user.
+#name = "Joe Mama"
+
+#Print the name:
+#print(name)
+
+#Print the name with an extra string:
+#print("Hello " + name)
+
+#Print the data type of the variable "name"
+#print(type(name))
+
+
+
+########################################################################################
+
+
+
+### First and Last name
+
+#Your first name:
+#first_name = "Joe"
+
+#Your last name:
+#last_name = "Mama"
+
+#Combining the first and last name into one:
+#full_name = first_name + " " + last_name
+
+#Printing the combined name with "Hello" at the start:
+#print("Hello " + full_name)
+
+
+
+########################################################################################
+
+
+### Integer(s)
+
+#The age of the user:
+#age = 13
+
+#Adding 1 year to the age (the hard way)
+#age = age + 1
+
+#Adding 1 year to the age (the easy way)
+#age += 1
+
+#Print the user's age:
+#print(age)
+
+#Print the age data type because I can
+#print(type(age))
+
+# int(integer) to str(string)
+#print("Your age is " + str(age))
+
+
+
+########################################################################################
+
+
+
+### float/floating point number (a decimal number)
+
+
+# This indicates that this is a floating point number(float) since it has a decimal
+#height = 250.5
+
+# Print the height:
+#print("Your height is: " + str(height)+"cm")
+
+# Print the height data type:
+#print(type(height))
+
+
+
+########################################################################################
+
+
+
+### Booleans
+
+
+#Returning human as "True" or "False":
+#human = True
+
+#Printing if human is true or false:
+#print("Are you a human? "+str(human))
+
+#Printing the human data type:
+#print(type(human))
+
+
+
+
+### a little "advanced" thing i made w the human boolean:
+
+
+
+#print("Are you a human?")
+#human = True
+#if human is True:
+#    print("Yes")
+#else:
+#    print("No")
+
+
+# ^ the above will prolly be used in the future ^
+
+
+
+
+
+########################################################################################
