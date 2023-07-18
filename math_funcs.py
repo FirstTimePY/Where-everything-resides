e3aa245b975a5650e813b07c759136eaf5eaa003
python beginner <139918720+FirstTimePY@users.noreply.github.com>
2023-07-19 01:02:20 +0300

    Add files via upload

---
+++ math_funcs.py
@@ -0,0 +1,29 @@
+import math
+
+pi = 3.142
+pi2 = -3.142
+x = 1
+y = 2
+z = 3
+
+#Rounds pi to the nearest number
+#print(round(pi))
+
+#Rounds pi to the nearest high number
+# print(math.ceil(pi))
+
+#Rounding down with math.floor
+# print(math.floor(pi))
+
+# Makes the negative number postive
+#print(abs(pi2))
+
+#Returns the variable "pi2" to the power of 2
+#print(pow(pi2,2))
+
+#Square root of the variable "pi"
+#print(math.sqrt(pi))
+
+# finding the smallest and largest value in the variables "x", "y" and "z"
+#print(max(x,y,z))
+#print(min(x,y,z))
