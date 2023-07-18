fd8b67f3942e7062bc838fe7c871280f70042aa3
python beginner <139918720+FirstTimePY@users.noreply.github.com>
2023-07-19 00:49:36 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# where the fun begins
+
+#create variables that lead to inputs
+name = input("What's your name?: ")
+age = int(input("How old are you?: "))
+height = float(input("how tall u blud (in cm)?: "))
+# too lazy to comment all this
+
+#printing:
+print("Hello there "+ name)
+#in the input field we convert "age" to an int, here we convert it to a str
+print("you are "+str(age)+" yrs old")
+print("You are "+str(height)+"cm tall")
