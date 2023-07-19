99e16716f40f70784872a0240c54732c1918b70c
python beginner <139918720+FirstTimePY@users.noreply.github.com>
2023-07-19 22:56:28 +0300

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,33 @@
+# list = used to store many items in a single var
+
+
+
+food = ["pineapple","cheeseburger","dog","apple"]
+
+#Replaces "pineapple" with sushi
+#food[0] = "sushi"
+
+#Prints the first food in the list
+#print(food[0])
+
+#Adds "ice cream" to the list of food
+#food.append("ice cream")
+
+#Removes "dog" from the food list
+#food.remove("dog")
+
+#Removes the last food in the list
+#food.pop()
+
+#Inserts another food/item in the index "2"
+#food.insert(2, "cow")
+
+#Sorts all food from a to z
+#food.sort()
+
+#Deletes everything in the list/deletes all food
+#food.clear()
+
+#Printing all the foods:
+for i in food:
+    print(i)
