441a22221ba1ddf12de03cb89e236cde865f1307
python beginner <139918720+FirstTimePY@users.noreply.github.com>
2023-07-19 23:24:45 +0300

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,29 @@
+# set = collection which is unordered, unindexed.
+# has no duplicate values
+
+hardware = {"ssd","gpu","mobo"}
+accessories = {"mouse","keyboard","mouse pad"}
+extras = {"mouse pad", "headphones"}
+
+
+# WILL ADD COMMENTS THAT EXPLAIN EVERYTHING SOON!!!
+# WILL ADD COMMENTS THAT EXPLAIN EVERYTHING SOON!!!
+# WILL ADD COMMENTS THAT EXPLAIN EVERYTHING SOON!!!
+
+
+
+
+
+
+# hardware.add("cpu")
+# hardware.remove("gpu")
+# hardware.clear()
+#hardware.update(accessories)
+# everything = hardware.union(accessories)
+#print(accessories.difference(extras))
+#print(accessories.intersection(extras))
+
+# for h in hardware:
+#     print(h)
+# for e in everything:
+#     print(e)
