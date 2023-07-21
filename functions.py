f6eec4276f84367cf1882bc5e3c756dde40d27e1
python beginner <139918720+FirstTimePY@users.noreply.github.com>
2023-07-21 09:16:10 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,34 @@
+# function = a block of code which is executed only when it is called
+
+#Defining the function with parameters
+def firstFunc(first, last, age):
+
+    #Normal printing:
+    print("Hello Python")
+    print("Enjoy the pizza thats under your carpet")
+
+    #Printing the name that is taken from the parameter
+    #print("stay away " + name)    
+    print("stay away " + first + " " + last)
+
+    #Printing the age as a string taken from the parameter
+    print("youre " + str(age) + "yrs old")
+
+
+#This changes "name" to contain "blud"
+#firstFunc("blud")
+
+#This changes "name" to contain "jeffery"
+#name="jeffery"
+
+#I think this is self explanatory
+#myname = "joe"
+
+#I think this is self explanatory
+firstFunc("Joe","Mama",17)
+
+#I think this is self explanatory
+#firstFunc(myname)
+
+#I think this is self explanatory
+#firstFunc(name)
