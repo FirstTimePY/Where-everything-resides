1585300f67570dfe2661c995c92f50331911538f
python beginner <139918720+FirstTimePY@users.noreply.github.com>
2023-07-21 09:20:24 +0300

    Add files via upload

---
+++ return.py
@@ -0,0 +1,12 @@
+# return statement = Funtions send PY values/objects back to the caller. These values/objects are known as the function's return value
+
+# Defining the function
+def multiply(num1, num2):
+    #Returning number 1 multiplied by number 2
+    return num1 * num2
+
+# X equals the numbers that are being multiplied
+x = multiply(6,8)
+
+# Printing the result:
+print(x)
