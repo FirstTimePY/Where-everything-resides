4553de5d41be2a7b8bd31d7682435bff1f93820a
python beginner <139918720+FirstTimePY@users.noreply.github.com>
2023-07-21 08:34:00 +0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -1,29 +1,40 @@
 # set = collection which is unordered, unindexed.
 # has no duplicate values
 
+# creating 3 different sets
 hardware = {"ssd","gpu","mobo"}
 accessories = {"mouse","keyboard","mouse pad"}
 extras = {"mouse pad", "headphones"}
 
 
-# WILL ADD COMMENTS THAT EXPLAIN EVERYTHING SOON!!!
-# WILL ADD COMMENTS THAT EXPLAIN EVERYTHING SOON!!!
-# WILL ADD COMMENTS THAT EXPLAIN EVERYTHING SOON!!!
 
 
-
-
-
-
+# Adding an item to the set "hardware"
 # hardware.add("cpu")
+
+# Removing an item from the set "hardware"
 # hardware.remove("gpu")
+
+# Clearing all items from the set "hardware"
 # hardware.clear()
+
+
 #hardware.update(accessories)
+
+# Merging the hardware set and accessories set
 # everything = hardware.union(accessories)
+
+# Printing the difference between accessories and extras
 #print(accessories.difference(extras))
+
+# Intersecting accessories and extras
 #print(accessories.intersection(extras))
 
+
+# Printing all items in the set "hardware"
 # for h in hardware:
 #     print(h)
+
+#Printing all the items in the merged set named "everything"
 # for e in everything:
 #     print(e)
