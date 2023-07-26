a1091d8cf065abfa00bbd51f5ec7d4ed91290602
python beginner <139918720+FirstTimePY@users.noreply.github.com>
2023-07-27 00:08:36 +0300

    Add files via upload

---
+++ reading_files.py
@@ -0,0 +1,12 @@
+#Lets read the contents of our test txt file!
+
+try:
+#With the listed test file below, we open it and read its contents and print them
+    with open("C:\\Users\\Fannan\\Desktop\\testfolder\\testfile.txt") as file:
+        print(file.read())
+#Exception, if the file is not found, print the following
+except FileNotFoundError:
+    print("file not found")
+
+# This checks if the file closes after its done being read, if so it will print "True" if not it will print "False"
+#print(file.closed)
