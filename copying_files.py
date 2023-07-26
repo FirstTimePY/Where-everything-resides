58a916785390175d2cfd11b957ce2a8cf4c2cd64
python beginner <139918720+FirstTimePY@users.noreply.github.com>
2023-07-27 00:21:26 +0300

    Add files via upload

---
+++ copying_files.py
@@ -0,0 +1,7 @@
+# copyfile() = copies contents of a file
+# copy() = copyfile() + permission mode + destination can be a dir
+# copy2() = copy() + copies metadata (files creation and modification times)
+
+import shutil
+
+shutil.copyfile('C:\\Users\\Fannan\\Desktop\\testfolder\\cool_file.txt', 'C:\\Users\\Fannan\\Desktop\\testfolder\\copied.txt') #source, destination - for copy() and copy2() the args are the same
