6aebcb19a4da4dd65cdff70ccc1dcc17865fb56b
python beginner <139918720+FirstTimePY@users.noreply.github.com>
2023-08-02 16:57:48 +0300

    Add files via upload

---
+++ funcs_to_vars.py
@@ -0,0 +1,20 @@
+# Assigning a Function to a Variable
+
+# We define a function called 'hello' that prints "Hello!"
+def hello():
+    print("Hello!")
+
+# Assign the function object 'hello' to a new variable 'hi'
+hi = hello
+
+# Call the function using the new variable 'hi'
+# This will execute the 'hello' function and print "Hello!"
+hi()
+
+
+# Assigning the 'print' function to a variable 'say'
+say = print
+
+# Call the function using the variable 'say'
+# This will execute the 'print' function and print "Woah! This is cool!"
+say("Woah! This is cool!")
